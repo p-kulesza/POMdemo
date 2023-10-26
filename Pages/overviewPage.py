e48c4938511b22e657fb3038e257dbd7466153a7
Piotr Kulesza <139692181+p-kulesza@users.noreply.github.com>
2023-10-26 13:04:04 +0200

    Update overviewPage.py
    26.10

--- Pages/overviewPage.py
+++ Pages/overviewPage.py
@@ -6,7 +6,9 @@ class OverviewPage():
         self.driver = driver
         self.button_cancel_id = "cancel"
         self.button_finish_id = "finish"
+        self.button_remove_css = ".btn.btn_secondary.btn_small.cart_button"
         self.overview_container_css = ".checkout_summary_container"
+        self.item_name_css = ".inventory_item_name"
         #whole payment info into text!!!
         #product name == product in cart
 
@@ -20,3 +22,10 @@ class OverviewPage():
         summary = self.driver.find_element(By.CSS_SELECTOR, self.overview_container_css)
         print(summary.text)
 
+    def get_item_name_text(self):
+        item_names = self.driver.find_elements(By.CSS_SELECTOR, self.item_name_css)
+        itemList = []
+        for item_name in item_names:
+            item_text = item_name.text
+            itemList.append(item_text)
+        return itemList
